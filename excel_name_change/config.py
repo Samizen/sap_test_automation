ec7f384e86e416b9db692da5b9044421ef494e0b
samizen <samitpdl@gmail.com>
2025-03-05 15:44:48 +0545

    Add test_playwright - test_create_receipt

--- excel_name_change/config.py
+++ excel_name_change/config.py
@@ -1,5 +1,10 @@
 VARS_TO_CHANGE= {
     "Scenario":"__changed",
     "Environment":"env",
-    "Enter":"_enter_" 
+    "Enter":"_enter_" ,
+    "Quantity : 10": "Quantity: 30",
+    "Bill To: 3020 (Texas Water Utilities, L.P.)": "Bill To: 3000",
+    "Purchase Group: Use default": "Purchase Group: Don't use default",
+    "Need-by Date: Today + 7 days" : "Need-by Date: voli",
+    "GL Account:  Use default": "GL"
 }
