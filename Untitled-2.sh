1572abf967ec93b9f093667d131de47b10ff731f
samizen <samitpdl@gmail.com>
2025-03-11 09:34:36 +0545

    Restuctured Folder and files

--- Untitled-2.sh
+++
@@ -1,3 +0,0 @@
-cd https://github.com/Samizen/sap_test_automation
-git config --global user.email "pandeysupriya3721@gmail.com"     
-git config --global user.name "spandey55"
--- test_playwright/test_end_to_end_lagacy.py
+++ test_playwright/test_end_to_end_lagacy.py
@@ -1,8 +1,8 @@
 # from test_create_pr import test_create_pr
 from test_playwright.test_create_pr import test_create_pr
 from test_playwright.test_create_receipt import test_create_receipt
-# from test_playwright.invoicelegacy import test_invoice_legacy
+from test_playwright.invoicelegacy import test_invoice_legacy
 
 test_create_pr()
 test_create_receipt()
-# test_end_to_end_legacy()
+test_invoice_legacy()
